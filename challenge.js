6d7a210da76fa7f6f54afd91e706c8ec376ad474
Matthew <mngz636@gmail.com>
2024-02-19 10:05:11 -0800

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,12 +1,33 @@
 
 function challenge(){
 
-e('challenge').innerHTML += "<a href=# class=block onclick='purchase(20);return false;' ><img src=trophy.jpg width=100px />5 rounds ($20)</a>"+
-  "<a href=# class=block onclick='purchase(40);return false;' ><img src=trophy.jpg width=100px />10 rounds ($40)</a>"+
-  "<a href=# class=block onclick='purchase(80);return false;' ><img src=trophy.jpg width=100px />15 rounds ($80)</a>";
+e('challenge').innerHTML += "<a href=# class=block onclick='purchase(100);return false;' ><img src=trophy.jpg width=100px />5 rounds (R100)</a>"+
+  "<a href=# class=block onclick='purchase(300);return false;' ><img src=trophy.jpg width=100px />10 rounds (R300)</a>"+
+  "<a href=# class=block onclick='purchase(500);return false;' ><img src=trophy.jpg width=100px />15 rounds (R500)</a>";
   
 }
 
-function purchase(price){
+function checkChallenge(){
+const urlParams = new URLSearchParams(window.location.search);
+const amount = urlParams.get('vv');
+const payment = urlParams.get('pp');
+if(amount && payment==1){
+e(amount).selected = true;
+play_p();
+}else if(payment==0){
+alert('Payment Cancelled');
+}
+}
 
+var HOME = "https://mngz47.github.io/F-Snake/index3.html";
+
+function purchase(price){
+this.price = price;
+this.product = "Flower Snake Challenge";
+this.return_url = HOME+"?pp=1";
+this.cancel_url =  HOME+"?pp=0";
+  
+  payfast();
+
+  setCookie(cname, cvalue, exdays)
 }
