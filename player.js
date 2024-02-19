3c0dbaeb0aaf1941b8efbdbd960332e14a8a0f32
Matthew <mngz636@gmail.com>
2024-02-19 05:54:45 -0800

    Update player.js

--- player.js
+++ player.js
@@ -1,18 +1,26 @@
+var player = 0;
+
 var players = ["Listro","Bobby","Matthew"];
 
 var player_speed = [2,8,12]; //the smaller the number greater the speed
 
-var precision = 20; //detection precision - axis size
+var precision = [20,30,40]; //detection precision - axis size
+
+function showPlayers(target){
+for(var a=0;a<precision.length;a++){
+   target.innerHTML += "<a href=# class=block onclick='iniPlayer("+a+");return false;' >"+players[a]+" ("+player_speed[a]+","+precision[a]+")</a>";
+}
+}
 
 async function findFlower(){
    
-   if(((e('food').offsetTop+precision)>e('head').offsetTop && (e('food').offsetTop-precision)<e('head').offsetTop)){ //detect y axis of food
+   if(((e('food').offsetTop+precision[player])>e('head').offsetTop && (e('food').offsetTop-precision[player])<e('head').offsetTop)){ //detect y axis of food
   if(e('food').offsetLeft>e('head').offsetLeft){
  d = "r";
   }else if(e('food').offsetLeft<e('head').offsetLeft){
  d = "l";
   }  
-   }else if(((e('food').offsetLeft+precision)>e('head').offsetLeft && (e('food').offsetLeft-precision)<e('head').offsetLeft)){ //detect x axis of food
+   }else if(((e('food').offsetLeft+precision[player])>e('head').offsetLeft && (e('food').offsetLeft-precision[player])<e('head').offsetLeft)){ //detect x axis of food
   if(e('food').offsetTop>e('head').offsetTop){ 
  d = "b";
   }else if(e('food').offsetTop<e('head').offsetTop){
@@ -22,6 +30,7 @@ async function findFlower(){
       
 }
 
-function iniPlayer(){
-setInterval(findFlower,500*(player_speed[0]));
+function iniPlayer(player){
+   this.player = player;
+setInterval(findFlower,500*(player_speed[player]));
 }
