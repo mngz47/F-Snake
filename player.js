a2b1e7650fc14532c71f2cd30fafe5377e16d1ce
Matthew <mngz636@gmail.com>
2024-02-19 03:55:41 -0800

    Update player.js

--- player.js
+++ player.js
@@ -4,11 +4,11 @@ var player_speed = [3,6,9]; //the smaller the number greater the speed
 
 async function findFlower(){
    
-   if(!((e('food').offsetTop+40)>e('head').offsetTop && (e('food').offsetTop-40)<e('head').offsetTop)){
+   if(!((e('food').offsetTop+20)>e('head').offsetTop && (e('food').offsetTop-20)<e('head').offsetTop)){
   if((e('food').offsetTop+20)>e('head').offsetTop){ 
- d = "t";
-  }else if((e('food').offsetTop-20)<e('head').offsetTop){
  d = "b";
+  }else if((e('food').offsetTop-20)<e('head').offsetTop){
+ d = "t";
   }
    }else if(!((e('food').offsetLeft+40)>e('head').offsetLeft && (e('food').offsetLeft-40)<e('head').offsetLeft)){
   if((e('food').offsetLeft+20)>e('head').offsetLeft){
