5ab277eb4316e2c9d533cbf3d432b076a3692f90
Matthew <mngz636@gmail.com>
2024-02-19 07:59:48 -0800

    Create challenge.js

---
+++ challenge.js
@@ -0,0 +1,12 @@
+
+function challenge(){
+
+e('challenge').innerHTML += "<a href=# class=block onclick='purchase(20);return false;' ><img src=trophy.jpg width=100px />5 rounds ($20)</a>"+
+  "<a href=# class=block onclick='purchase(40);return false;' ><img src=trophy.jpg width=100px />10 rounds ($40)</a>"+
+  "<a href=# class=block onclick='purchase(80);return false;' ><img src=trophy.jpg width=100px />15 rounds ($80)</a>";
+  
+}
+
+function purchase(price){
+
+}
