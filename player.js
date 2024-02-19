d254313b0fd687a5a242acb4b659980aaa88e62a
Matthew <mngz636@gmail.com>
2024-02-19 04:10:05 -0800

    Update player.js

--- player.js
+++ player.js
@@ -1,16 +1,18 @@
 var players = ["Listro","Bobby","Matthew"];
 
-var player_speed = [3,6,9]; //the smaller the number greater the speed
+var player_speed = [10,6,9]; //the smaller the number greater the speed
+
+var precision = 40;
 
 async function findFlower(){
    
-   if(!((e('food').offsetTop+70)>e('head').offsetTop && (e('food').offsetTop-70)<e('head').offsetTop)){
+   if(!((e('food').offsetTop+precision)>e('head').offsetTop && (e('food').offsetTop-precision)<e('head').offsetTop)){
   if(e('food').offsetTop>e('head').offsetTop){ 
  d = "b";
   }else if(e('food').offsetTop<e('head').offsetTop){
  d = "t";
   }
-   }else if(!((e('food').offsetLeft+70)>e('head').offsetLeft && (e('food').offsetLeft-70)<e('head').offsetLeft)){
+   }else if(!((e('food').offsetLeft+precision)>e('head').offsetLeft && (e('food').offsetLeft-precision)<e('head').offsetLeft)){
   if(e('food').offsetLeft>e('head').offsetLeft){
  d = "r";
   }else if(e('food').offsetLeft<e('head').offsetLeft){
