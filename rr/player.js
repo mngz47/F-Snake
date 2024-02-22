cbbd72671de836161e2e9eb1dac39dde43bf06f2
Matthew <mngz636@gmail.com>
2024-02-21 23:35:35 -0800

    Update player.js

--- rr/player.js
+++ rr/player.js
@@ -106,5 +106,7 @@ async function trailBiasObstacle(){
 function iniPlayer(player){
      this.player = player;
    setInterval(findFlower,500*(player_speed[player]));
-  setInterval(trailBiasObstacle,500*(player_speed[player])); 
+   setInterval(trailBiasObstacle,500*(player_speed[player]));
+   setInterval(moveAroundObstacle,250*(player_speed[player])); 
+
 }
