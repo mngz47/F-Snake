26471713bd90c1e8e2d39ed6a1869305d6e781bd
Matthew <mngz636@gmail.com>
2024-02-27 06:43:36 -0800

    Update effects.js

--- rr/effects.js
+++ rr/effects.js
@@ -1,6 +1,44 @@
+//stimulation and game confirmation
+
 function playTheme(){
   
 var audio = new Audio('sounds/snake_music.mp3');
 audio.play();
   
+}
+
+function food_effect(){
+
+}
+
+function obstacle_effect(){
+
+}
+
+function results_effect(){
+
+}
+
+
+//respond to user effects
+
+function shake_effect(){
+
+}
+
+function vanish_effect(){
+
+}
+
+function fall_effect(){
+
+}
+
+function breath_effect(){
+
+
+}
+
+function pulse_effect(){
+
 }
