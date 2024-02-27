d19f8fe3a903c8cc61d8970f0632bfed2f22947c
Matthew <mngz636@gmail.com>
2024-02-27 05:39:54 -0800

    Update user_experience.js

--- rr/user_experience.js
+++ rr/user_experience.js
@@ -47,9 +47,7 @@ e('canvas').style.opacity = "1";
 
 function show_challenge_winner(p1points,p1stage,p1seconds,p1moves,p2points,p2stage,p2seconds,p2moves,winner){
 
-return 
-    "<div class=row style='background-image:url(\"images/celebrate.gif\");text-align:center;font-size:2em;display:block;margin-top:150px;' >"+
-   
+return "<div class=row style='background-image:url(\"images/celebrate.gif\");font-size:0.6em;margin-top:150px;' >"+
     "<div  class='col-sm-6 block' >"+
     
     "<img src='images/trophy.jpg' width=150px />"+
